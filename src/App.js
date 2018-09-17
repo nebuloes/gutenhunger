ad1792998639582fd3296d2cb6bff37c326c3ea6
Johanna <meier.johanna@outlook.de>
2018-09-17 14:26:31 +0200

    Show recipes from local json in App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react'
 import './App.css'
+import { recipes } from './recipes.json'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {recipes.map((recipe, i) => (
+          <div key={i}>
+            <h1>{recipe.RezeptName}</h1>
+            <h2>
+              {recipe.SchwierigkeitsgradName}, {recipe.Minuten} Minuten
+            </h2>
+            <h3>Zutaten</h3>
+            <ul>
+              {recipe.Zutaten.split(',').map((zutat, i) => (
+                <li key={i}>{zutat}</li>
+              ))}
+            </ul>
+            <div>{recipe.Zubereitung}</div>
+          </div>
+        ))}
       </div>
     )
   }
