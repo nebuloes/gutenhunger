dafab72390f80a1dba0c1b44df762d4ba7c16808
Johanna <meier.johanna@outlook.de>
2018-09-18 17:17:43 +0200

    Add arrow element, Link to single recipe only around title in RecipeCard now

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react'
 import { recipes } from '../recipes.json'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import RecipeCard from './RecipeCard'
 import Recipe from './Recipe'
 
 class App extends Component {
   renderRecipeCards = () => {
-    return recipes.map((recipe, i) => (
-      <Link key={i} to={'/recipe/' + recipe.RezeptID}>
-        <RecipeCard key={recipe.RezeptID} recipe={recipe} />
-      </Link>
+    return recipes.map(recipe => (
+      <RecipeCard key={recipe.RezeptID} recipe={recipe} />
     ))
   }
 
--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
+import arrow from '../arrow.png'
 
 export default class Recipe extends Component {
   static propTypes = {
@@ -9,7 +11,10 @@ export default class Recipe extends Component {
     const { recipe } = this.props
     return (
       <div data-test-id="Recipe-card">
-        <h1>{recipe.RezeptName}</h1>
+        <Link to={'/recipe/' + recipe.RezeptID}>
+          <h1>{recipe.RezeptName}</h1>
+        </Link>
+        <img src={arrow} alt="" width="30px" />
       </div>
     )
   }
