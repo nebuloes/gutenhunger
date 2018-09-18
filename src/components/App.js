93ff4aef19b1be02c1eb90249ad7c96eef3eab59
Johanna <meier.johanna@outlook.de>
2018-09-18 13:55:42 +0200

    Add single recipe page with own url ending as recipe ID

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react'
 import { recipes } from '../recipes.json'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import RecipeCard from './RecipeCard'
+import Recipe from './Recipe'
 
 class App extends Component {
-
-renderRecipeCards = () => {
-  return (
-    recipes.map((recipe, i) => (
-    <RecipeCard key={i} recipe={recipe} /> 
+  renderRecipeCards = () => {
+    return recipes.map((recipe, i) => (
+      <Link key={i} to={'/recipe/' + recipe.RezeptID}>
+        <RecipeCard key={recipe.RezeptID} recipe={recipe} />
+      </Link>
     ))
-  )
-}
+  }
+
+  renderRecipe = ({ match }) => {
+    const recipeID = Number(match.params.id)
+    const foundRecipe = recipes.find(recipe => recipe.RezeptID === recipeID)
+    return <Recipe recipe={foundRecipe} />
+  }
 
   render() {
     return (
       <Router>
-      <div className="App">
-      <Route exact path="/" render={this.renderRecipeCards} />
-      <Route path="/recipe" render={this.renderRecipe} />
-      </div>
+        <div className="App">
+          <Route exact path="/" render={this.renderRecipeCards} />
+          <Route path="/recipe/:id" render={this.renderRecipe} />
+        </div>
       </Router>
     )
   }
