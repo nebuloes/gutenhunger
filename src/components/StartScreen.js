4eccb1e1bd5e7b2899cea6356edb69a38e0a001a
Johanna <meier.johanna@outlook.de>
2018-10-02 12:47:58 +0200

    Install react-search-input, add to StartScreen

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,75 +1,47 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
+import SearchInput, { createFilter } from 'react-search-input'
 
 import { recipes } from '../recipes.json'
 import RecipeCardContainer from '../containers/RecipeCardContainer'
 
-export default class StartScreen extends Component {
-  static propTypes = {
-    onSubmit: PropTypes.func,
-  }
+const KEYS_TO_FILTERS = [
+  'RezeptName',
+  'SchwierigkeitsgradName',
+  'Minuten',
+  'Zutaten',
+  'Zubereitung',
+  'dest.name',
+]
 
+export default class SearchScreen extends Component {
   state = {
-    inputValue: '',
-    search: false,
-  }
-
-  updateInputValue = event => {
-    const input = event.target
-    this.setState({
-      inputValue: input.value,
-    })
-  }
-
-  checkForEnterButton = event => {
-    const { inputValue } = this.state
-    if (event.key === 'Enter' && inputValue !== '') {
-      this.setState({ search: true })
-    }
-  }
-
-  showRecipesFromSearch() {
-    const { inputValue } = this.state
-    const search = recipes.map(recipe => Object.values(recipe))
-    console.log(search)
-    return search.includes(inputValue) ? <div>success</div> : <div>fail</div>
-    // recipes.map(recipe => (
-    //   <RecipeCardContainer key={recipe.RezeptID} recipe={recipe} />
-    // ))
-  }
-
-  showRecipesOrSearchResult() {
-    return this.state.search
-      ? this.showRecipesFromSearch()
-      : recipes.map(recipe => (
-        <RecipeCardContainer key={recipe.RezeptID} recipe={recipe} />
-      ))
+    searchTerm: '',
   }
 
   render() {
+    const filteredRecipes = recipes.filter(
+      createFilter(this.state.searchTerm, KEYS_TO_FILTERS)
+    )
+
     return (
-      <React.Fragment>
-        <input
-          value={this.state.inputValue}
-          onChange={this.updateInputValue}
-          onKeyUp={this.checkForEnterButton}
+      <div>
+        <SearchInput
+          className="search-input"
+          onChange={this.searchUpdated}
+          placeholder="Rezept suchen"
         />
-        {this.showRecipesOrSearchResult()}
+        {filteredRecipes.map(recipe => {
+          return <RecipeCardContainer key={recipe.RezeptID} recipe={recipe} />
+        })}
         <Link data-test-id="Link-to-fridge" to="/fridge">
           Go to fridge
         </Link>
-      </React.Fragment>
+      </div>
     )
   }
-}
 
-// const { inputValue } = this.state
-// const foundRecipes = recipes.filter(recipe => recipe.includes(inputValue))
-// this.state.search
-//       ? foundRecipes.map(recipe => (
-//         <RecipeCardContainer key={recipe.RezeptID} recipe={foundRecipes} />
-//       ))
-//       : recipes.map(recipe => (
-//         <RecipeCardContainer key={recipe.RezeptID} recipe={recipe} />
-//       ))
+  searchUpdated = term => {
+    this.setState({ searchTerm: term })
+  }
+}
